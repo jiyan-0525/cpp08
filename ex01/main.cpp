499c7844ed83c648329df68a57436f7428fc05fe
ThePrimeagen <the.primeagen@aol.com>
2026-08-15 13:20:16 +0200

    updateman

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,25 +1,16 @@
 #include "Span.hpp"
 
-int main() {
+int main()
+{
     Span sp = Span(5);
+    
     sp.addNumber(6);
     sp.addNumber(3);
     sp.addNumber(17);
     sp.addNumber(9);
     sp.addNumber(11);
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
 
-    std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-    std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-
-    Span sp2 = Span(10000);
-    std::vector<int> numbers;
-    for (int i = 0; i < 10000; ++i) {
-        numbers.push_back(i * 2);
-    }
-    
-    sp2.addNumber(numbers.begin(), numbers.end());
-
-    std::cout << "Shortest span in large span: " << sp2.shortestSpan() << std::endl;
-    std::cout << "Longest span in large span: " << sp2.longestSpan() << std::endl;
     return 0;
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,37 +1,34 @@
 #include "MutantStack.hpp"
 
-int main () {
+int main()
+{
     MutantStack<int> mstack;
+
     mstack.push(5);
     mstack.push(17);
-    mstack.push(20);
-    std::cout << "Top element: " << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << "Size: " << mstack.size() << std::endl;
 
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+    
     mstack.push(3);
     mstack.push(5);
-    mstack.push(37);
+    mstack.push(737);
+    //[...]
     mstack.push(0);
-    std::cout << "interating forward: " << std::endl;
+
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
-    for (; it != ite; ++it) {
+
+    ++it;
+    --it;
+    while (it != ite)
+    {
         std::cout << *it << std::endl;
+        ++it;
     }
-
-    std::cout << "interwating reverse: " << std::endl;
-    MutantStack<int>::reverse_iterator rit = mstack.rbegin();
-    MutantStack<int>::reverse_iterator rite = mstack.rend();
-    for (; rit != rite; ++rit) {
-        std::cout << *rit << std::endl;
-    }
-
-    MutantStack<int> copy(mstack);
-    std::cout << "Copying stack: " << std::endl;
-    MutantStack<int>::iterator cit = copy.begin();
-    MutantStack<int>::iterator cite = copy.end();
-    for (; cit != cite; ++cit) {
-        std::cout << *cit << std::endl;
-    }
+    std::stack<int> s(mstack);
+    return 0;
 }
